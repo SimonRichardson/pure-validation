a6a7bb49c7bae5533776fb0f7ac6a8dbd08965e6
SimonRichardson <stickupkid@gmail.com>
2015-04-21 12:02:31 +0100

    Local storage is now working

--- src/Main.purs
+++ src/Main.purs
@@ -12,7 +12,7 @@ import Data.AddressBook
 import Data.AddressBook.UI
 
 import Control.Monad.Eff
-import Control.Monad.Eff.DOM (DOM())
+import Control.Monad.Eff.DOM
 import Control.Monad.Eff.Alert
 import Control.Monad.Eff.Storage
 
@@ -54,8 +54,82 @@ instance formDataIsForeign :: IsForeign FormData where
       , cellPhone  : cellPhone
       } 
 
-main :: forall eff. Eff (trace :: Trace, dom :: DOM | eff) Unit
+toFormData :: Person -> FormData
+toFormData (Person p@{ address = (Just (Address a))
+                     , phones = [ PhoneNumber pn1
+                                , PhoneNumber pn2
+                                , PhoneNumber pn3
+                                ]
+                     }) =
+  FormData { firstName   : p.firstName
+           , lastName    : p.lastName
+
+           , street      : a.street
+           , city        : a.city
+           , state       : a.state
+
+           , homePhone   : pn1.number
+           , cellPhone   : pn2.number
+           }
+
+
+updateForm :: forall eff. String -> String -> Eff (dom :: DOM | eff) Unit
+updateForm sel value = do
+  Just element <- querySelector sel
+  setValue value element
+  return unit
+
+loadSavedData :: forall eff. Eff (trace :: Trace, alert :: Alert, dom :: DOM, storage :: Storage | eff) Unit
+loadSavedData = do
+  item <- getItem "person"
+
+  let
+    savedData :: F (Maybe FormData)
+    savedData = do
+      jsonOrNull <- read item
+      traverse readJSON (runNull jsonOrNull)
+
+  case savedData of
+    Left err -> alert $ "Unable to read saved form data: " ++ show err
+    Right Nothing -> return unit
+    Right (Just (FormData o)) -> do
+      updateForm "#inputFirstName" o.firstName
+      updateForm "#inputLastName"  o.lastName
+
+      updateForm "#inputStreet"    o.street
+      updateForm "#inputCity"      o.city
+      updateForm "#inputState"     o.state
+
+      updateForm "#inputHomePhone" o.homePhone
+      updateForm "#inputCellPhone" o.cellPhone
+
+      return unit
+
+validateAndSaveEntry :: forall eff. Eff (trace :: Trace, alert :: Alert, dom :: DOM, storage :: Storage | eff) Unit
+validateAndSaveEntry = do
+  trace "Running validators"
+
+  errorsOrResult <- validateControls
+
+  case errorsOrResult of
+    Left errs -> alert $ "There are " ++ show (length errs) ++ " validation errors."
+    Right result -> do
+      setItem "person" $ stringify $ toForeign $ toFormData result
+      alert "Saved"
+
+  return unit
+
+main :: forall eff. Eff (trace :: Trace, alert :: Alert, dom :: DOM, storage :: Storage | eff) Unit
 main = do
+  trace "Loading data from lcoal storage"
+  loadSavedData
+
   trace "Attaching event handlers"
   setupEventHandlers
+
+  Just saveButton <- querySelector "#saveButton"
+
+  addEventListener "click" validateAndSaveEntry saveButton
+
+  return unit
 
